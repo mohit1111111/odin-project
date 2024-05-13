18c5df094972dc2030678aa9f74ca32854f4f59c
mohit1111111 <97935102+mohit1111111@users.noreply.github.com>
2024-05-13 17:24:08 +0530

    created script andd gui

--- foundation/rock_paper_scissors/scripts.js
+++ foundation/rock_paper_scissors/scripts.js
@@ -13,6 +13,7 @@ function getHumanChoice(){
     return input;
 }
 function playRound(choiseh,choisec){
+    choiseh = choiseh.toUpperCase();
     let winner;
     if(choiseh == choisec){
         return "";
@@ -31,6 +32,7 @@ function playRound(choiseh,choisec){
     }
     return winner;
 }
+/*
 function playGame(){
     let humanScore = 0;
     let computerScore = 0;
@@ -66,3 +68,58 @@ function playGame(){
     }
 }
 playGame();
+*/
+
+const body = document.querySelector("body");
+
+body.style.height = window.innerHeighteight;
+
+const btns = document.querySelectorAll("button");
+
+const para = document.querySelector("p");
+
+const h3 = document.querySelector("h3");
+
+let humanScore = 0;
+let computerScore = 0;
+
+btns.forEach(btn => {
+    btn.addEventListener("click",()=>{
+        h3.textContent = "";   
+
+        {
+            let humanSelection = btn.id;
+            let computerSelection = getComputerChoises();
+            let winner = playRound(humanSelection,computerSelection);
+            if(winner == "Human"){
+                let message = "you win! "+humanSelection+" beats "+computerSelection+".";
+                humanScore++;
+                para.textContent = message;
+            }
+            else if(winner == "Computer"){
+                let message = "you lose! "+computerSelection+" beats "+humanSelection+".";
+                computerScore++;
+                para.textContent = message;
+
+    
+            }
+            else if(winner ==""){
+                para.textContent = "Draw";
+            }
+        }
+        if(humanScore+computerScore==5){
+            if(humanScore>computerScore){
+                h3.textContent = "you win! your score "+humanScore;
+            }
+            else if(computerScore>humanScore){
+                h3.textContent = "you lose! your score "+humanScore;
+            }
+            else{
+                h3.textContent = "draw your score"+humanScore;
+            }
+                humanScore = 0;
+                computerScore =  0;
+
+       }
+    })
+});
