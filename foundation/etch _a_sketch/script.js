ce19582e978a7907901f3f9f2a98855dfab329ee
mohit1111111 <97935102+mohit1111111@users.noreply.github.com>
2024-05-14 12:11:29 +0530

    added etch_a_sketch

---
+++ foundation/etch _a_sketch/script.js
@@ -0,0 +1,42 @@
+const gbox = document.getElementById("gridbox");
+
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+
+function randomColor(){
+    let h = Math.random()*100;
+    let s = Math.random()*100;
+    let l = Math.random()*90;
+
+    return "hsl("+h+","+80+"%,"+l+"%)"
+}
+
+function createGrid(num){
+    for(let i = 0;i<num;i++){
+        const gridrow = document.createElement("div");
+        gridrow.setAttribute("class","gridrow");
+        for(let j = 0;j<num;j++){
+            const box = document.createElement("div");
+            box.addEventListener("mouseenter",()=>{
+                box.style.backgroundColor = randomColor();
+            })
+            gridrow.appendChild(box);
+        }
+        gbox.appendChild(gridrow);
+
+    }
+
+}
+
+
+btn.addEventListener("click",()=>{
+
+    gbox.replaceChildren();
+    let value = parseInt(input.value);
+    input.value = "";
+    if(value > 100)value = 100;
+    createGrid(value);
+
+})
+
+createGrid(5);
