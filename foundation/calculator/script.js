5b1e0a2b57bc24cb3bf20cb513171a0f0401e9db
mohit1111111 <97935102+mohit1111111@users.noreply.github.com>
2024-05-26 17:34:29 +0530

    added keyboard support

--- foundation/calculator/script.js
+++ foundation/calculator/script.js
@@ -4,51 +4,138 @@ let operation = "";
 let secondNo = 0.0;
 let decimalFlag = false;
 
+
 const screen = document.getElementById("screen");
 
 function updateScreen(){
     screen.textContent = currentNo;
 }
 
+//perform operations and add and remove it
+
+function addOperation(op){
+    if(operation == ""){
+        firstNo = parseFloat(currentNo);
+        operation = op;
+        currentNo = 0; 
+        screen.textContent = "";
+        decimalFlag=false;
+    }
+    else {
+        secondNo = parseFloat(currentNo);
+        currentNo = operate();
+        updateScreen();
+        
+        firstNo = parseFloat(currentNo);
+        currentNo = 0;
+        operation = "";
+        operation = op;
+        decimalFlag=false;
+
+
+    }
+}
+
 
 function  operate(){
     let result = 0;
-    if(operation=="+")result = Math.round((firstNo+secondNo)*10)/10;
+    if(operation=="+")result = Math.round((firstNo+secondNo)*100)/100;
 
-    if(operation=="*")result = Math.round((firstNo*secondNo)*10)/10;
+    if(operation=="*")result = Math.round((firstNo*secondNo)*100)/100;
 
-    if(operation=="-")result = Math.round((firstNo-secondNo)*10)/10;
+    if(operation=="-")result = Math.round((firstNo-secondNo)*100)/100;
 
     if(operation=="/"){
         if(secondNo==0){
             return "lamao";
         }
-        result = Math.round((firstNo/secondNo)*10)/10;;
+        result = Math.round((firstNo/secondNo)*100)/100;;
     }
     return result;
 }
 
-const persentage = document.getElementById("per");
-
-persentage.addEventListener('click',()=>{
+function performPersentage(){
     secondNo = parseFloat(currentNo);
 
     if(firstNo>0&&secondNo>0){
-        if(secondNo%1==0){
-        secondNo = firstNo*parseFloat("0."+(secondNo/10));
+        if(secondNo%1==0 && secondNo<10){
+            secondNo = firstNo*parseFloat("0.0"+(secondNo));
+        }
+        else if(secondNo%1==0){
+            secondNo = firstNo*parseFloat("0."+(secondNo/10));
         }
         else{
-        secondNo = firstNo*(secondNo/10);
+            secondNo = firstNo*(secondNo/10);
         }
         currentNo = operate();
         updateScreen();        
         firstNo = parseFloat(currentNo);
         currentNo = 0;
         operation = "";
-
-
     }
+}
+
+
+
+//take input related to operation
+
+
+const persentage = document.getElementById("per");
+
+persentage.addEventListener('click',()=>{
+    performPersentage();
+
+
+    
 });
+
+
+
+const operator = document.querySelectorAll("#operator");
+
+operator.forEach((opn)=>{
+    opn.addEventListener("click",()=>{
+        addOperation(opn.textContent)}
+)
+});
+
+function eqOperation(){
+    if(operation != ""){
+        secondNo = parseFloat(currentNo);
+        currentNo = operate();
+        updateScreen();
+        firstNo = parseFloat(currentNo);
+        operation = "";
+    }
+}
+
+
+
+const equal = document.getElementById("equal");
+
+equal.addEventListener("click",()=>{
+    eqOperation();
+    
+}
+);
+
+
+//all clear operation
+
+const allClear = document.getElementById("AC");
+
+allClear.addEventListener("click",()=>{
+    firstNo=0;
+    secondNo=0;
+    decimalFlag=false;
+    operation="";
+    currentNo = 0;
+    screen.textContent = "";
+});
+
+//add no and dots
+
+
 
 const numbtn = document.querySelectorAll("#number");
 
@@ -66,60 +153,6 @@ numbtn.forEach((btn)=>
 
 
 
-
-const operator = document.querySelectorAll("#operator");
-
-operator.forEach((opn)=>{
-    opn.addEventListener("click",()=>{
-        if(operation == ""){
-            firstNo = parseFloat(currentNo);
-            operation = opn.textContent;
-            currentNo = 0; 
-            screen.textContent = "";
-            decimalFlag=false;
-        }
-        else {
-            secondNo = parseFloat(currentNo);
-            currentNo = operate();
-            updateScreen();
-            
-            firstNo = parseFloat(currentNo);
-            currentNo = 0;
-            operation = "";
-            operation = opn.textContent;
-            decimalFlag=false;
-
-
-        }
-        
-    })
-});
-
-const equal = document.getElementById("equal");
-
-equal.addEventListener("click",()=>{
-    if(operation != ""){
-            secondNo = parseFloat(currentNo);
-            currentNo = operate();
-            updateScreen();
-            firstNo = parseFloat(currentNo);
-            operation = "";
-    }
-
-    
-}
-);
-
-const allClear = document.getElementById("AC");
-
-allClear.addEventListener("click",()=>{
-    firstNo=0;
-    secondNo=0;
-    decimalFlag=false;
-    operation="";
-    currentNo = 0;
-    screen.textContent = "";
-});
 const dot = document.getElementById("dot");
 
 dot.addEventListener("click",()=>{
@@ -129,6 +162,8 @@ dot.addEventListener("click",()=>{
         decimalFlag = true;
     }
 });
+
+
 
 const sign = document.getElementById("sign");
 
@@ -142,4 +177,26 @@ sign.addEventListener('click',()=>{
     updateScreen();
 })
 
+//take keyboard input
+
+document.addEventListener("keydown",function(e){
+    if(e.key>=0 && e.key <=9){
+        currentNo=currentNo+e.key;
+        updateScreen();
+    }
+    else if(e.key=="+")addOperation(e.key);
+    else if(e.key=="-")addOperation(e.key);
+    else if(e.key=="*")addOperation(e.key);
+    else if(e.key=="/")addOperation(e.key);
+    else if(e.key=="%")performPersentage();
+    else if(e.key=="Enter"||e.key=="=")eqOperation();
+    else if(e.key=="Backspace"){
+        firstNo=0;
+        secondNo=0;
+        decimalFlag=false;
+        operation="";
+        currentNo = 0;
+        screen.textContent = "";
+    }
+});
 
